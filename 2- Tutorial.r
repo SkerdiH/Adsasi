865121233bc69050d22bb94e5328b020ddc19276
Skerdi Haviari <skerdihaviari@gmail.com>
2025-06-16 12:00:21 +0200

    Create 2- Tutorial.r

---
+++ 2- Tutorial.r
@@ -0,0 +1,44 @@
+# Checking that the base function works
+readline(prompt="Press [enter] to start the tutorial")
+cat("Let us first check that our individual simulator function works with command : ","\n",sep="")
+readline(prompt="Press [enter] to continue")
+cat("simulate_one_trial(500)","\n",sep="")
+cat(simulate_one_trial(500),"\n",sep="")
+cat("simulate_one_trial(500)","\n",sep="")
+cat(simulate_one_trial(500),"\n",sep="")
+cat("simulate_one_trial(500)","\n",sep="")
+cat(simulate_one_trial(500),"\n",sep="")
+
+readline(prompt="Press [enter] to continue")
+cat("Now let's do a batch of 1000 trials and compute the observed power with","\n",sep="")
+readline(prompt="Press [enter] to continue")
+cat("mean(replicate(1000,simulate_one_trial(500)))","\n",sep="")
+cat(mean(replicate(1000,simulate_one_trial(500))),"\n",sep="")
+
+readline(prompt="Press [enter] to continue")
+cat("Interesting, but we seem to have overshot, and we would like to avoid trial and error","\n",sep="")
+readline(prompt="Press [enter] to continue")
+
+# Running adsasi
+cat("So let's try with the adsasi algorithm (check the plotting device), with command ; ","\n",sep="")
+cat("adsasi(simulate_one_trial,0.90)  # 0.90 for 90% power","\n",sep="")
+readline(prompt="Press [enter] to continue")
+proposal = adsasi(simulate_one_trial,0.90)
+
+readline(prompt="Press [enter] to continue")
+cat("The analytical sample size for the tutorial function is 44 patients. How did adsasi do ?","\n",sep="")
+readline(prompt="Press [enter] to continue")
+
+# Running adsasi again
+cat("Let's do it again in case the plots were too fast or the window dimensions too small the first time","\n",sep="")
+readline(prompt="Resize the plotting window and press [enter] to continue")
+proposal = adsasi(simulate_one_trial,0.90)
+
+# Running adsasi one last time
+cat("Want to change one of the arguments of the function you wrote ? Put it in there : ","\n",sep="")
+cat("adsasi(simulate_one_trial,0.90,effsize=.5)  # changing the effsize parameter","\n",sep="")
+readline(prompt="Press [enter] to continue")
+
+# Finish
+cat("Do not hesitate to display the adsasi function itself, it is heavily annotated","\n",sep="")
+readline(prompt="Press [enter] to finish the tutorial")
